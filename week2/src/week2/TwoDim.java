97693291c5a653359cf9fcbf9c77ade5546a37fe
2eunbi <70017526+2eunbi@users.noreply.github.com>
2021-03-11 16:46:39 +0900

    firstcommit

---
+++ week2/src/week2/TwoDim.java
@@ -0,0 +1,12 @@
+package week2;
+
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+		int sale[][] = new int[][]{{1,2,3,4},{5,6,7,8}};
+			for(int i=0; i<2; i++) 
+				for(int k = 0; k < 4; k++)
+					System.out.println(sale[i][k]);
+	}
+}
